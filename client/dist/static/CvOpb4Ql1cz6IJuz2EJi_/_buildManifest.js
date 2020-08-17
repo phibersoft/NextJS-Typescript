dc0c576d4e6ed129e4828f83bd36cba7da1e35d3
phibersoft <phibersoft>
2020-08-17 16:52:55 +0300

    Cross Env

---
+++ client/dist/static/CvOpb4Ql1cz6IJuz2EJi_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-547c5dfeff09cf802505.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa9f225803eac1f77e35.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
